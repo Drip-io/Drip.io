25ad87f8edde7da41fecbc90ef7d2d9783819691
MathSouz <mmedeiros.cbp@gmail.com>
2022-10-29 12:26:20 -0300

    fixed conflicts

--- app/src/main/java/com/dripio/presentation/payments/PaymentsActivity.kt
+++ app/src/main/java/com/dripio/presentation/payments/PaymentsActivity.kt
@@ -6,11 +6,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.dripio.domain.entity.Payment
 import com.dripio.extensions.formatToMonthYear
 import com.dripio.extensions.toMoneyStringWithComma
+import com.dripio.extensions.visibleOrGone
 import com.dripio.presentation.base.startPaymentEditor
 import com.dripio.presentation.bottomSheet.PaymentDetailBottomSheet
 import com.example.dripio.databinding.ActivityPaymentsBinding
 import com.google.android.material.datepicker.MaterialDatePicker
-import com.example.dripio.extensions.visibleOrGone
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.util.*
 
